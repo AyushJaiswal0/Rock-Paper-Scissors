2416a1f9806b70b6a8a211b29a23994977f597ef
AyushJaiswal0 <jaiswalayush05@proton.me>
2024-07-12 15:02:52 +0530

    Improve interaction for UI

--- script.js
+++ script.js
@@ -4,98 +4,173 @@ function getComputerChoice()
     const choice = Math.random() * 10;
     if(choice <= 3)
     {
-        return "Rock";
+        return "rock";
     }
     else if(choice <= 6)
     {
-        return "Paper";
+        return "paper";
     }
-    return "Scissor";
+    return "scissor";
 }
 
-function getHumanChoice()
+function humanChoiceIcon(choice)
 {
-    const choice = prompt("Enter rock, paper or scissor:");
-    return choice;
+    if(choice == "rock")
+    {
+        humanIcon.innerHTML = '<img src="./icons/humanChoice/rock-sign.jpeg" alt="rock sign">';
+    }
+    else if(choice == "paper")
+    {
+        humanIcon.innerHTML = '<img src="./icons/humanChoice/paper-sign.jpeg" alt="paper sign">';
+    }
+    else if(choice == "scissor")
+    {
+        humanIcon.innerHTML = '<img src="./icons/humanChoice/scissor-sign.jpeg" alt="scissor sign">';
+    }
+    else
+    {
+        humanIcon.innerHTML = '<img src="./icons/human-icon.jpg" alt="human icon">';
+    }
+}
+
+function computerChoiceIcon(choice)
+{
+    if(choice == "rock")
+    {
+        computerIcon.innerHTML = '<img src="./icons/computerChoice/invert-rock-sign.jpeg" alt="rock sign">';
+    }
+    else if(choice == "paper")
+    {
+        computerIcon.innerHTML = '<img src="./icons/computerChoice/invert-paper-sign.jpeg" alt="paper sign">';
+    }
+    else if(choice == "scissor")
+    {
+        computerIcon.innerHTML = '<img src="./icons/computerChoice/invert-scissor-sign.jpeg" alt="scissor sign">';
+    }
+    else
+    {
+        computerIcon.innerHTML = '<img src="./icons/computer-icon.png" alt="computer icon">';
+    }
+}
+
+function colorChange(win = 0)
+{
+    if(win == 1)
+    {
+        humanIcon.setAttribute("style", "border-color : green");
+        computerIcon.setAttribute("style", "border-color : red");
+    }
+    else if(win == 0)
+    {
+        humanIcon.setAttribute("style", "border-color : red");
+        computerIcon.setAttribute("style", "border-color : green");
+    }
+    else
+    {
+        humanIcon.setAttribute("style", "border-color : black");
+        computerIcon.setAttribute("style", "border-color : black");
+    }
 }
 
 function playRound(humanChoice , computerChoice)
 {
+    humanChoiceIcon(humanChoice);
+    computerChoiceIcon(computerChoice);
     if(humanChoice.toUpperCase() == computerChoice.toUpperCase())
     {
-        console.log("Tie, both chose " + computerChoice);
+        result.textContent = ("Tie, both chose " + computerChoice);
+        colorChange(0.5);
     }
     else if(humanChoice.toUpperCase() == "ROCK")
     {
-        if(computerChoice == "Scissor")
+        if(computerChoice == "scissor")
         {
-            console.log("You win! Rock beats Scissor");
+            colorChange(1);
+            result.textContent = ("You win! Rock beats Scissor");
             humanScore += 1;
         }
         else 
         {
-            console.log("You lose! Paper beats Rock");
+            colorChange();
+            result.textContent = ("You lose! Paper beats Rock");
             computerScore += 1;
         }
     }
     else if(humanChoice.toUpperCase() == "PAPER")
     {
-            if(computerChoice == "Scissor")
+            if(computerChoice == "scissor")
             {
-                console.log("You lose! Scissor beats Paper");
+                colorChange();
+                result.textContent = ("You lose! Scissor beats Paper");
                 computerScore += 1;
             }
             else 
             {
-                console.log("You win! Paper beats Rock");
+                colorChange(1);
+                result.textContent = ("You win! Paper beats Rock");
                 humanScore += 1;
             }
     }
     else if(humanChoice.toUpperCase() == "SCISSOR")
     {
-        if(computerChoice == "Paper")
+        if(computerChoice == "paper")
         {
-            console.log("You win! Scissor beats Paper");
+            colorChange(1);
+            result.textContent = ("You win! Scissor beats Paper");
             humanScore += 1;
         }
         else 
         {
-            console.log("You lose! Rock beats Paper");
+            colorChange();
+            result.textContent = ("You lose! Rock beats Scissor");
             computerScore += 1;
         }
     }
-    else
+    const score = document.querySelector("#score");
+    score.textContent = humanScore + " - " + computerScore;
+    if(humanScore == 5)
     {
-        console.log("Invalid Input");
-        playRound(getHumanChoice(), getComputerChoice());
+        result.textContent = "You win!😊";
+    }
+    else if(computerScore == 5)
+    {
+        result.textContent = "You lose! Computer wins 😙"
     }
 }
 
-function playGame()
+function reset()
 {
-
-    for(let i = 0; i < 5; i++)
-    {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
-    console.log("You " + humanScore + " : " + computerScore + " Computer");
-    if(humanScore > computerScore)
-    {
-        console.log("You win!");
-    }
-    else if(humanScore < computerScore)
-    {
-        console.log("You lose!");
-    }
-    else
-    {
-        console.log("Tied!")
-    }
+    humanScore = 0;
+    computerScore = 0;
+    result.textContent = "Play again!!";
+    computerChoiceIcon("reset");
+    humanChoiceIcon("reset");
+    humanIcon.setAttribute("style", "border-color : black");
+    computerIcon.setAttribute("style", "border-color : black");
 }
 
 let humanScore = 0;
 let computerScore = 0;
+const result = document.querySelector("#result");
+const score = document.querySelector("#score");
+score.textContent = humanScore + " - " + computerScore;
+const humanIcon = document.querySelector("#human-choice");
+const computerIcon = document.querySelector("#computer-choice");
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        console.log(button.id);
+        const computerSelection = getComputerChoice();
+        if(humanScore == 5 || computerScore == 5 || button.id == "reset")
+        {
+            reset();
+            score.textContent = humanScore + " - " + computerScore;
+            
+        }
+        else
+        {
+            playRound(button.id, computerSelection);
+        }
+    });
+});
 
-playGame();
